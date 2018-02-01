f77eb6584baae1ee138e25c945d70eab2c8ab6e8
php 10 <php.10@salsicha.cec.ime.usp.br>
2018-01-31 22:44:20 -0200

    adicionando layout e correções

---
+++ resources/views/cursos/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layout')
+
+ @section('content')
+  <div class="row">
+    <div class="col-md-6">
+
+      <h3>Cursos</h3>
+
+      <ul class="list-group">
+        @foreach ($cursos as $curso)
+          <li class="list-group-item"><a href="/cursos/{{ $loop->index }}">{{ $curso }}</a></li>
+        @endforeach
+      </ul>
+
+    </div>
+  </div>
+ @endsection
---
+++ resources/views/cursos/show.blade.php
@@ -0,0 +1,9 @@
+ @extends('layout')
+
+ @section('content')
+  <div class="row">
+      <div class="col-md-6">
+        <h4>{{ $curso }}</h4>
+      </div>
+  </div>
+ @endsection
